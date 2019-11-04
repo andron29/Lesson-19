6d0ed2e86978957fc83f2bccc8feedafed387967
andrewstamb <andrei.stambul@gmail.com>
2019-11-04 21:28:35 +0200

    add change

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
-class Menu {
-    constructor(elem) {
-      this._elem = elem;
-      elem.onclick = this.onClick.bind(this); 
-    }
-    but1() {
-      alert('You clicked First button');
-      console.log('capturing', 'this.tagName');
-    }
-    but2() {
-      alert('You clicked Second button');
-      console.log('capturing', 'this.tagName');
-    }
-    but3() {
-      alert('You clicked Third button');
-      console.log('capturing', 'this.tagName')
-    }
-    onClick(event) {
-      let action = event.target.dataset.action;
-      if (action) {
-        this[action]();
-    }
-  };
-}
+var str1_ = "You clicked First button",
+      changeText = function()  {
+          document.querySelector('h3').textContent = str1_;
+      }
+  document.getElementById('but1').addEventListener('click', changeText, false);
 
-new Menu(menu);
+  var str2_ = 'You clicked Second button',
+      changeText = function()  {
+          document.querySelector('h3').textContent = str2_;
+      }
+  document.getElementById('but2').addEventListener('click', changeText, false);
+
+  var str3_ = 'You clicked Third button',
+      changeText = function()  {
+          document.querySelector('h3').textContent = str3_;
+      }
+  document.getElementById('but3').addEventListener('click', changeText, false); 
